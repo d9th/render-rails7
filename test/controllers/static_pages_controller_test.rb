eaf490c58e8aff5181a274a8a70a636f4e24855c
d9th <gravitate.karma+github@gmail.com>
2023-02-21 00:41:39 +0900

    Changed readme.

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,6 +3,8 @@ require "test_helper"
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get hello" do
     get root_url
+    assert_select 'title', 'Hello Render'
+    assert_select 'h1', "Hello Render from Rails 7"
     assert_response :success
   end
 end
